c20aef18d008d35284735a35849f074a08c8b5db
Fedor Sharaputo <sharaputo@icloud.com>
2021-03-22 11:40:08 +0300

    form final fixes

--- es6/contact-form.js
+++ es6/contact-form.js
@@ -1,8 +1,8 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', function () {
-  const form = document.querySelector('#contact_form');
-  const successPopup = document.querySelector('#success_popup');
-  const warning = document.querySelector('.contact__warning');
+  const form = document.querySelector('#contact_form'),
+    successPopup = document.querySelector('#success_popup'),
+    warning = document.querySelector('.contact__warning');
 
   form.addEventListener('submit', formSend);
 
@@ -12,17 +12,11 @@ document.addEventListener('DOMContentLoaded', function () {
     let inputs = document.querySelectorAll('#contact_form input');
     for (let i = 0; i < inputs.length; i++) {
       const input = inputs[i];
-
-      input.onfocus = function () {
-        input.classList.remove('_error');
-        warning.classList.remove('active');
-      };
     }
 
     let error = formValidate(form);
 
     if (error === 0) {
-      // form.classList.add('_sending');
       let response = await fetch('sendmail.php', {
         method: 'POST',
         body: formData,
@@ -30,13 +24,12 @@ document.addEventListener('DOMContentLoaded', function () {
       if (response.ok) {
         let result = await response.json();
         form.reset();
-        // form.classList.remove('_sending');
         successPopup.classList.add('active');
       } else {
         alert('Error!');
       }
     } else {
-      warning.classList.add('active');
+      addWarning();
     }
 
     function formValidate(form) {
@@ -45,7 +38,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
       for (let i = 0; i < formReq.length; i++) {
         const input = formReq[i];
-        formRemoveError(input);
 
         if (input.classList.contains('_email')) {
           if (emailCheck(input)) {
@@ -63,16 +55,30 @@ document.addEventListener('DOMContentLoaded', function () {
             error++;
           }
         }
+
+        input.onfocus = function () {
+          input.classList.remove('_error');
+          removeWarning();
+        };
       }
 
       return error;
     }
+
     function formAddError(input) {
       input.classList.add('_error');
     }
     function formRemoveError(input) {
       input.classList.remove('_error');
     }
+
+    function addWarning() {
+      warning.classList.add('active');
+    }
+    function removeWarning() {
+      warning.classList.remove('active');
+    }
+
     function emailCheck(input) {
       return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
     }
