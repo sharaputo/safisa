e9a7f6a1c28d052ce7cfbe49dd923698e3c80192
Fedor Sharaputo <sharaputo@icloud.com>
2021-03-24 13:42:21 +0300

    links update

--- sendmail.php
+++ sendmail.php
@@ -10,19 +10,19 @@ $mail->Charset = 'UTF-8';
 $mail->IsHTML(true);
 
 $mail->setFrom('info@safisa.shop', 'Safisa website');
-$mail->addAddress('geibgb@gmail.com');
+$mail->addAddress('sharaputo@icloud.com');
 $mail->Subject = 'New email form Safisa website';
 
 $body = '<h1>User Contact Details</h1>';
 
 if(trim(!empty($_POST['name']))){
-  $body = '<p><strong>Name:</strong> '.$_POST['name'].'</p>';
+  $body .= '<p><strong>Name:</strong> '.$_POST['name'].'</p>';
 }
 if(trim(!empty($_POST['phone']))){
-  $body = '<p><strong>Phone:</strong> '.$_POST['phone'].'</p>';
+  $body .= '<p><strong>Phone:</strong> '.$_POST['phone'].'</p>';
 }
 if(trim(!empty($_POST['email']))){
-  $body = '<p><strong>Email:</strong> '.$_POST['email'].'</p>';
+  $body .= '<p><strong>Email:</strong> '.$_POST['email'].'</p>';
 }
 
 $mail->Body = $body;
